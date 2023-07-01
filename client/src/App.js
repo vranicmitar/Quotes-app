6506b5cfa6c1fe3caea87c8c215da8291a07d675
vranicmitar <mitarvranic0@gmail.com>
2023-07-01 20:25:03 +0200

    #nece

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Quotes from "./components/Quotes/Quotes";
 import HomePage from "./components/HomePage/HomePage";
+import Login from "./pages/Login/Login";
+import { AppContext } from "./context/AppContext";
 
 export default function App() {
+  const { token, setToken } = useContext(AppContext);
+  useEffect(() => {
+    const localToken = localStorage.getItem("token");
+    setToken(localToken);
+  }, []);
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/" element={<Login />} />
         <Route path="/quotes" element={<Quotes />}></Route>
       </Routes>
     </>
---
+++ client/src/context/AppContext.js
@@ -0,0 +1,20 @@
+import React, { createContext, useState } from "react";
+
+const AppContext = createContext();
+
+function ContextProvider({ children }) {
+  const [token, setToken] = useState(null);
+
+  const values = {
+    token,
+    setToken,
+  };
+
+  return (
+    <>
+      <AppContext.Provider value={values}>{children}</AppContext.Provider>
+    </>
+  );
+}
+
+export { AppContext, ContextProvider };
