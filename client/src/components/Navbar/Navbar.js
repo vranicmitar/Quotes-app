039e3868d4754256d56c05761c3ee26fdec695be
vranicmitar <mitarvranic0@gmail.com>
2023-07-01 17:30:04 +0200

    #

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <header className="bg-white h-20">
-      <h1>Mitar</h1>
+    <header className="bg-yellow-100 h-20 flex justify-between items-center">
+      <h1 className="text-3xl ml-6">GoodReads</h1>
+      <nav className="w-1/5  flex justify-around  mr-6 ">
+        <h1 className="text-red-700 text-lg">Quotes</h1>
+        <button className="text-red-700 text-lg">LogOut</button>
+      </nav>
     </header>
   );
 }
